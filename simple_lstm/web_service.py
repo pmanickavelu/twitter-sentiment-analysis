8445f2a57c8c7d09c9973171d85516736949a75f
Prabhu Manickavelu <prabhumanickavelu@Prabhus-MacBook-Pro.local>
2019-12-25 00:00:25 +0530

    added docker file

--- simple_lstm/web_service.py
+++ simple_lstm/web_service.py
@@ -12,43 +12,44 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import model_from_json
 
 import re, pickle,os
-# import json
 
-# session = tf.Session()
-model = None
+with sess.as_default():
+    with graph.as_default():
+        model = model_from_json(open("model.json","r").read())
+        model.load_weights('model.h5')
+
 tokenizer = pickle.load(open("tokenizer.pkl","rb"))
 max_text_length = pickle.load(open("max_text_length.pkl","rb"))
 
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
-    global model
-    global graph
-    global sess
-    if model == None:
-        with sess.as_default():
-            with graph.as_default():
-                model = model_from_json(open("model.json","r").read())
-                print (model.summary())
-                model.load_weights('model.h5')
 
-    content = request.get_json()
-    print(type(content["text"]))
+    o_texts, texts = clean_data(request.get_json()['text'])
+    predicts = make_prediction(texts)
+    results = build_results(predicts,o_texts,texts)
+    
+    return jsonify(results)
+def clean_data(text):
     o_texts = []
-    if type(content["text"]) == str:
-        o_texts = [content["text"]]
-    elif type(content["text"]) == list:
-        o_texts = content["text"]
+    if type(text) == str:
+        o_texts = [text]
+    elif type(text) == list:
+        o_texts = text
     texts = o_texts
     for i in range(len(texts)):
         texts[i] = texts[i].lower()
         texts[i] = re.sub('[^a-zA-z0-9\s]','',texts[i])
         texts[i] = texts[i].replace('rt',' ')
+    return o_texts,texts
+def make_prediction(texts):
+    global sess
+    global graph
+    global model
     with sess.as_default():
         with graph.as_default():
-            predicts = model.predict(pad_sequences(tokenizer.texts_to_sequences(texts),max_text_length).reshape(len(texts),max_text_length),batch_size=1)
-    print(predicts)
-    print(predicts[np.argmax(predicts)])
+            return model.predict(pad_sequences(tokenizer.texts_to_sequences(texts),max_text_length).reshape(len(texts),max_text_length),batch_size=1)
+def build_results(predicts,o_texts,texts):
     results = []
     for i in range(len(texts)):
         sentiment = "Positive"
@@ -62,6 +63,6 @@ def index():
                 "text": o_texts[i],
                 "score":float(predicts[i][np.argmax(predicts[i])])
                 })
-    return jsonify(results)
+    return results
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)
